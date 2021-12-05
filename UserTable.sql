b3afa39a847490d2782efca022b3956aded17110
Ranjithabr5117 <72100129+Ranjithabr5117@users.noreply.github.com>
2021-12-05 18:09:49 +0530

    Add files via upload

---
+++ UserTable.sql
@@ -0,0 +1,10 @@
+create table User(
+UserId integer,
+FirstName varchar2(50),
+LastName varchar2(50),
+EmailId varchar2(15),
+Password varchar(25)
+);
+
+insert into User(UserId,FirstName,LastName,EmailId,Password) values (1,'Ranjithaa','Rangappa','Ranjitha.Rangappa@socgen.com','1234');
+insert into User(UserId,FirstName,LastName,EmailId,Password) values (2,'Ranjitha','R','Ranjitha@gmail.com','r@1234');
